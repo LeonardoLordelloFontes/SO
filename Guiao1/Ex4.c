2764ea96524eb5910bb25f5e17db7bbfd642ba56
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-02-21 01:26:12 +0000

    Create Ex4.c

---
+++ Guiao1/Ex4.c
@@ -0,0 +1,38 @@
+ssize_t readln(int fd, char *line, size_t size) {
+    char c;
+    ssize_t bytes_read = 0;
+    while (read(fd, &c, 1) > 0 && c != '\n') {
+        if (bytes_read >= size) {
+            fprintf(stderr, "buffer size is not enough to store the line\n");
+            return -1;
+        }
+        line[bytes_read] = c;
+        bytes_read++;
+    }
+    return bytes_read;
+}
+
+int main (int argc, char *argv[]) {
+    if (argc > 2) {
+        fprintf(stderr, "usage: %s [FILE]\n", argv[0]);
+        return 1;
+    }
+    int fd = 0;
+    if (argc == 2) {
+        fd = open(argv[1], O_RDONLY);
+    }
+    if (fd == -1) {
+        perror(argv[1]);
+        return 1;
+    }
+    char buffer[1024], n_buffer[32];
+    int bytes, counter = 1;
+    while ((bytes = readln(fd, buffer, 1024)) > 0) {
+        sprintf(n_buffer, "%6d  ", counter);
+        write(1, n_buffer, 8);
+        write(1, buffer, bytes);
+        write(1, "\n", 1);
+        counter++;
+    }
+    return 0;
+}
