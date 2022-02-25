b29529131ecc06bb81caa2dde1af53a9be3a9244
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-02-25 19:12:53 +0000

    Update Ex3.c

--- Guiao1/Ex3.c
+++ Guiao1/Ex3.c
@@ -1,11 +1,7 @@
 ssize_t readln(int fd, char *line, size_t size) {
     char c;
     ssize_t bytes_read = 0;
-    while (read(fd, &c, 1) > 0 && c != '\n') {
-        if (bytes_read >= size) {
-            fprintf(stderr, "buffer size is not enough to store the line\n");
-            return -1;
-        }
+    while (read(fd, &c, 1) > 0 && c != '\n' && bytes_read >= size) {
         line[bytes_read] = c;
         bytes_read++;
     }
