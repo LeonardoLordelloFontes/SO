e16173a68f2e4a192f00a42d35bb3d1b6b12d091
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-02-25 19:15:15 +0000

    Update Ex4.c

--- Guiao1/Ex4.c
+++ Guiao1/Ex4.c
@@ -6,7 +6,7 @@
 ssize_t readln(int fd, char *line, size_t size) {
     char c;
     ssize_t bytes_read = 0;
-    while (read(fd, &c, 1) > 0 && c != '\n' && bytes_read >= size) {
+    while (read(fd, &c, 1) > 0 && c != '\n' && bytes_read < size) {
         line[bytes_read] = c;
         bytes_read++;
     }
