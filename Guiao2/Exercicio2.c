52848d5517846bd32ca9251b00c56850ae571b42
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-03-17 02:58:34 +0000

    Create Exercicio2.c

---
+++ Guiao2/Exercicio2.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main() {
+	char buffer[64];
+	pid_t pid;
+	int status;
+	
+	if ((pid = fork()) == 0) {
+		int size = snprintf(buffer, 64, "processo: %d, pai: %d\n", getpid(), getppid());
+		write(1, buffer, size);
+		_exit(0);
+	}
+	wait(&status);
+	int size = snprintf(buffer, 64, "processo: %d, pai: %d, filho: %d\n", getpid(), getppid(), pid);
+
+	return (WIFEXITED(status) && write(1, buffer, size) != -1) ? 0 : 1;
+}
