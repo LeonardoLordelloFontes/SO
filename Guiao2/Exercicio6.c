3b633f5b60f7015bea65f9f1fcef3f7d3fa9adf5
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-03-17 01:44:49 +0000

    Create Exercicio6.c

---
+++ Guiao2/Exercicio6.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LINHAS 10
+#define COLUNAS 1000
+#define RANGE 500
+
+int** getRandomIntMatriz(int linhas, int colunas, int range) {
+    int** matriz = malloc(sizeof(int*) * linhas);
+    srand(time(NULL));
+
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = malloc(sizeof(int) * colunas);
+        for (int j = 0; j < colunas; j++) {
+            matriz[i][j] = rand() % range;
+        }
+    }
+    return matriz;
+}
+
+int main(int argc, char *argv[]) {
+    int** matriz = getRandomIntMatriz(LINHAS, COLUNAS, RANGE);
+    int status;
+    int encontrado = 0;
+    int naLinha[LINHAS] = {0};
+    pid_t pids[LINHAS];
+    pid_t pid;
+    
+    for (int i = 0; i < LINHAS; i++) {
+        if ((pid = fork()) == 0) {
+            for (int j = 0; j < COLUNAS; j++) {
+                if (atoi(argv[1]) == matriz[i][j]) {
+                	_exit(1);
+                }
+            }
+            _exit(0);
+        }
+        else {
+        	pids[i] = pid;
+        }
+    }
+    
+    for (int i = 0; i < LINHAS; i++) {
+    	waitpid(pids[i], &status, 0);
+    	if (!WIFEXITED(status)) 
+            break;
+        if (WEXITSTATUS(status)) {
+        	naLinha[i] = 1;
+        	encontrado = 1;
+        }
+    }
+    
+    if (WIFEXITED(status)) {
+    	if (encontrado) {
+		char buffer[LINHAS * 10 + 64];
+		int size = snprintf(buffer, 64, "o número %d foi encontrado nas linhas: ", atoi(argv[1]));
+		for (int i = 0; i < LINHAS; i++) {
+			if (naLinha[i])
+				size += snprintf(buffer + size, sizeof(buffer) - size, "%d ", i); 
+		}
+		size += snprintf(buffer + size, sizeof(buffer) - size, "\n");
+		write(1, buffer, size);
+    	}
+    	else {
+		char buffer[64];
+		int size = snprintf(buffer, 64, "o número %d NÃO foi encontrado\n", atoi(argv[1]));
+		write(1, buffer, size);
+	}
+    }
+    
+    return WIFEXITED(status) ? 0 : 1;
+}
