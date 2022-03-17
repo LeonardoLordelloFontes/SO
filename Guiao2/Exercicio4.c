b576e870faf71222a84c1ff6c3b92750e8c69ff4
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2022-03-17 03:11:06 +0000

    Create Exercicio4.c

---
+++ Guiao2/Exercicio4.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {	
+	char buffer[64];
+	int status;
+		
+	for (int i = 0; i < 10; i++) {
+		pid_t pid = fork();
+		if (pid == 0) {
+			int size = snprintf(buffer, 64, "processo: %d, pai: %d\n", getpid(), getppid());
+			(write(1, buffer, size) != -1) ? _exit(0) : _exit(1);
+		}
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		pid_t pid = wait(&status);
+		if (!WIFEXITED(status) || WEXITSTATUS(status))
+			return 1;
+	}
+	
+	return 0;
+}
